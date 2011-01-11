4b7529f008179374edebef0b79804959732e0d1d
Justin Collins <jcollins@attinteractive.com>
2011-01-10 16:09:13 -0800

    Check link_to for XSS opportunities

--- lib/checks/check_cross_site_scripting.rb
+++ lib/checks/check_cross_site_scripting.rb
@@ -42,6 +42,8 @@ class CheckCrossSiteScripting < BaseCheck
     @models = tracker.models.keys
     @inspect_arguments = OPTIONS[:check_arguments]
 
+    CheckLinkTo.new(checks, tracker).run_check
+
     tracker.each_template do |name, template|
       @current_template = template
 
@@ -207,5 +209,116 @@ class CheckCrossSiteScripting < BaseCheck
     end
     exp
   end
+end
 
+#This _only_ checks calls to link_to
+class CheckLinkTo < CheckCrossSiteScripting
+  IGNORE_METHODS = IGNORE_METHODS - [:link_to]
+
+  def run_check
+    #Ideally, I think this should also check to see if people are setting
+    #:escape => false
+    methods = tracker.find_call [], :link_to 
+
+    @models = tracker.models.keys
+    @inspect_arguments = OPTIONS[:check_arguments]
+
+    methods.each do |call|
+      process_result call
+    end
+  end
+
+  def process_result result
+    #Have to make a copy of this, otherwise it will be changed to
+    #an ignored method call by the code above.
+    call = result[-1] = result[-1].dup
+
+    @matched = false
+
+    return if call[3][1].nil?
+
+    #Only check first argument for +link_to+, as the second
+    #will *usually* be a record or escaped.
+    first_arg = process call[3][1]
+
+    type, match = has_immediate_user_input? first_arg
+
+    if type
+      case type
+      when :params
+        message = "Unescaped parameter value in link_to"
+      when :cookies
+        message = "Unescaped cookie value in link_to"
+      else
+        message = "Unescaped user input value in link_to"
+      end
+
+      unless duplicate? result
+        add_result result
+
+        warn :result => result,
+          :warning_type => "Cross Site Scripting", 
+          :message => message,
+          :confidence => CONFIDENCE[:high]
+      end
+
+    elsif not OPTIONS[:ignore_model_output] and match = has_immediate_model?(first_arg)
+      method = match[2]
+
+      unless duplicate? result or IGNORE_MODEL_METHODS.include? method
+        add_result result
+
+        if MODEL_METHODS.include? method or method.to_s =~ /^find_by/
+          confidence = CONFIDENCE[:high]
+        else
+          confidence = CONFIDENCE[:med]
+        end
+
+        warn :result => result,
+          :warning_type => "Cross Site Scripting", 
+          :message => "Unescaped model attribute in link_to",
+          :confidence => confidence
+      end
+
+    elsif @matched
+      if @matched == :model and not OPTIONS[:ignore_model_output]
+        message = "Unescaped model attribute in link_to"
+      elsif @matched == :params
+        message = "Unescaped parameter value in link_to"
+      end
+
+      if message and not duplicate? result
+        add_result result
+
+        warn :result => result, 
+          :warning_type => "Cross Site Scripting", 
+          :message => message,
+          :confidence => CONFIDENCE[:med]
+      end
+    end
+  end
+
+  def process_call exp
+    @mark = true
+    actually_process_call exp
+    exp
+  end
+
+
+  def actually_process_call exp
+    return if @matched
+
+    target = exp[1]
+    if sexp? target
+      target = process target.dup
+    end
+
+    #Bare records create links to the model resource,
+    #not a string that could have injection
+    if model_name? target and context == [:call, :arglist]
+      return exp
+    end
+
+    super
+  end
 end
