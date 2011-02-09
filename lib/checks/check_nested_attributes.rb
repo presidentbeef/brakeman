f5418defaf2e6de68fb21126ecd951fd10aaeaa6
Justin Collins <jcollins@attinteractive.com>
2011-02-09 14:00:49 -0800

    Add check for vulnerability in nested attributes
    http://groups.google.com/group/rubyonrails-security/browse_thread/thread/f9f913d328dafe0c

---
+++ lib/checks/check_nested_attributes.rb
@@ -0,0 +1,32 @@
+require 'checks/base_check'
+require 'processors/lib/find_call'
+
+class CheckNestedAttributes < BaseCheck
+  Checks.add self
+
+  def run_check
+    version = tracker.config[:rails_version]
+
+    if (version == "2.3.9" or version == "3.0.0") and uses_nested_attributes?
+      message = "Vulnerability in nested attributes (CVE-2010-3933). Upgrade to Rails version "
+
+      if version == "2.3.9"
+        message << "2.3.10"
+      else
+        message << "3.0.1"
+      end
+
+      warn :warning_type => "Nested Attributes",
+        :message => message,
+        :confidence => CONFIDENCE[:high]
+    end
+  end
+
+  def uses_nested_attributes?
+    tracker.models.each do |name, model|
+      return true if model[:options][:accepts_nested_attributes_for]
+    end
+
+    false
+  end
+end
