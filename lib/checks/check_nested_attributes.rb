a943ad78aecf583f252986b8881846a56e4a06e1
Justin Collins <jcollins@attinteractive.com>
2011-02-09 16:08:15 -0800

    Add link to nested attrib vulnerability

--- lib/checks/check_nested_attributes.rb
+++ lib/checks/check_nested_attributes.rb
@@ -1,6 +1,8 @@
 require 'checks/base_check'
 require 'processors/lib/find_call'
 
+#Check for vulnerability in nested attributes in Rails 2.3.9 and 3.0.0
+#http://groups.google.com/group/rubyonrails-security/browse_thread/thread/f9f913d328dafe0c
 class CheckNestedAttributes < BaseCheck
   Checks.add self
 
