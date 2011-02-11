71d3030c67f000ce5415d04130969e4ec00f7051
Justin Collins <jcollins@attinteractive.com>
2011-02-11 10:55:33 -0800

    Add check for CSRF weakness - CVE-2011-0447
    http://groups.google.com/group/rubyonrails-security/browse_thread/thread/2d95a3cc23e03665

--- lib/checks/check_forgery_setting.rb
+++ lib/checks/check_forgery_setting.rb
@@ -1,6 +1,9 @@
 require 'checks/base_check'
 
-#Checks that +protect_from_forgery+ is set in the ApplicationController
+#Checks that +protect_from_forgery+ is set in the ApplicationController.
+#
+#Also warns for CSRF weakness in certain versions of Rails:
+#http://groups.google.com/group/rubyonrails-security/browse_thread/thread/2d95a3cc23e03665
 class CheckForgerySetting < BaseCheck
   Checks.add self
 
@@ -19,6 +22,20 @@ class CheckForgerySetting < BaseCheck
       warn :controller => :ApplicationController, 
         :warning_type => "Cross-Site Request Forgery", 
         :message => "'protect_from_forgery' should be called in ApplicationController", 
+        :confidence => CONFIDENCE[:high]
+
+    elsif version_between? "2.1.0", "2.3.10"
+      
+      warn :controller => :ApplicationController, 
+        :warning_type => "Cross-Site Request Forgery",
+        :message => "CSRF protection is flawed in #{tracker.config[:rails_version]} (CVE-2011-0447). Upgrade to 2.3.11 or apply patches",
+        :confidence => CONFIDENCE[:high]
+
+    elsif version_between? "3.0.0", "3.0.3"
+
+      warn :controller => :ApplicationController, 
+        :warning_type => "Cross-Site Request Forgery",
+        :message => "CSRF protection is flawed in #{tracker.config[:rails_version]} (CVE-2011-0447). Upgrade to 3.0.4",
         :confidence => CONFIDENCE[:high]
     end
   end
