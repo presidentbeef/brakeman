fb7cef1b579b0e5ca4f465e122b6f56ced6a9c37
Justin Collins <jcollins@attinteractive.com>
2011-02-18 11:38:11 -0800

    Add code to tab output

--- lib/report.rb
+++ lib/report.rb
@@ -635,7 +635,7 @@ class Report
       [:model_warnings, "Model"], [:template_warnings, "Template"]].map do |meth, category|
 
       checks.send(meth).map do |w|
-        "#{file_for w}\t#{w.line}\t#{w.warning_type}\t#{category}\t#{w.message}\t#{TEXT_CONFIDENCE[w.confidence]}"
+        "#{file_for w}\t#{w.line}\t#{w.warning_type}\t#{category}\t#{w.format_message}\t#{TEXT_CONFIDENCE[w.confidence]}"
       end.join "\n"
 
     end.join "\n"
