1e5ba0f26072a358fd5e8933c4548c6988789534
Justin Collins <jcollins@attinteractive.com>
2011-08-17 16:38:49 -0700

    Check for response splitting vulnerability
    http://groups.google.com/group/rubyonrails-security/t/6ffc93bde0298768

---
+++ lib/checks/check_response_splitting.rb
@@ -0,0 +1,18 @@
+require 'checks/base_check'
+require 'processors/lib/find_call'
+
+#Warn about response splitting in Rails versions before 2.3.13
+#http://groups.google.com/group/rubyonrails-security/browse_thread/thread/6ffc93bde0298768
+class CheckResponseSplitting < BaseCheck
+  Checks.add self
+
+  def run_check
+    if version_between?('2.3.0', '2.3.13')
+
+      warn :warning_type => "Response Splitting",
+        :message => "Versions before 2.3.14 have a vulnerability content type handling allowing injection of headers. Upgrade or apply patches as needed.",
+        :confidence => CONFIDENCE[:med]
+
+    end
+  end
+end
