7d0cc8dccafad02230ef70359802c1f93ad98409
Justin Collins <jcollins@attinteractive.com>
2011-08-17 16:38:07 -0700

    Check for filter skipping vulnerability
    http://groups.google.com/group/rubyonrails-security/t/3420ac71aed312d6

---
+++ lib/checks/check_filter_skipping.rb
@@ -0,0 +1,27 @@
+require 'checks/base_check'
+require 'processors/lib/find_call'
+
+#Check for filter skipping vulnerability
+#http://groups.google.com/group/rubyonrails-security/browse_thread/thread/3420ac71aed312d6
+class CheckFilterSkipping < BaseCheck
+  Checks.add self
+
+  def run_check
+    if version_between?('3.0.0', '3.0.9') and uses_arbitrary_actions?
+
+      warn :warning_type => "Default Routes",
+        :message => "Versions before 3.0.10 have a vulnerability which allows filters to be bypassed. Upgrade or apply patches as needed.",
+        :confidence => CONFIDENCE[:high]
+    end
+  end
+
+  def uses_arbitrary_actions?
+    tracker.routes.each do |name, actions|
+      if actions == :allow_all_actions
+        return true
+      end
+    end
+
+    false
+  end
+end
