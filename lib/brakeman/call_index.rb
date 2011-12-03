14ebf8a6949be1c7a4e31b0884fb67c8cfba1b7a
Justin Collins <jcollins@attinteractive.com>
2011-12-02 19:04:34 -0800

    Make sure methods are symbols when searching
    in CallIndex

--- lib/brakeman/call_index.rb
+++ lib/brakeman/call_index.rb
@@ -108,14 +108,15 @@ class Brakeman::CallIndex
       elsif methods.length > 1
         calls_by_methods methods
       else
-        calls_by_method[methods.first]
+        @calls_by_method[methods.first.to_sym]
       end
     else
-      @calls_by_method[method]
+      @calls_by_method[method.to_sym]
     end
   end
 
   def calls_by_methods methods
+    methods = methods.map { |m| m.to_sym }
     calls = []
 
     methods.each do |method|
