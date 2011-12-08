83ca09f6503eafa3240fce58b919625d27b9cf3d
Justin Collins <jcollins@attinteractive.com>
2011-12-08 10:03:17 -0800

    Fix path for ruby_parser

--- lib/ruby_parser/ruby_parser.rb
+++ lib/ruby_parser/ruby_parser.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH << File.dirname(File.expand_path(__FILE__))
+$LOAD_PATH.unshift File.dirname(File.expand_path(__FILE__))
+
 require 'ruby18_parser'
 require 'ruby19_parser'
 require 'ruby_parser_extras'
