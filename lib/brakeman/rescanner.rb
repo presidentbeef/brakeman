e2367af8a614a219de3acc0e8e5cb8489044f757
Justin Collins <jcollins@attinteractive.com>
2012-01-13 10:43:16 -0800

    Add debug output to Rescanner

--- lib/brakeman/rescanner.rb
+++ lib/brakeman/rescanner.rb
@@ -45,6 +45,8 @@ class Brakeman::Rescanner < Brakeman::Scanner
 
     SCAN_ORDER.each do |type|
       paths_by_type[type].each do |path|
+        warn "Rescanning #{path} as #{type}" if tracker.options[:debug]
+
         if rescan_file path, type
           @changes = true
         end
