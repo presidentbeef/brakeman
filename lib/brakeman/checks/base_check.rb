8e04ccb318fffe0ec0e6da65bbb526b71fe36498
Justin Collins <jcollins@attinteractive.com>
2012-01-16 12:57:58 -0800

    Remove BaseCheck#debug_info

--- lib/brakeman/checks/base_check.rb
+++ lib/brakeman/checks/base_check.rb
@@ -20,7 +20,6 @@ class Brakeman::BaseCheck < SexpProcessor
     @tracker = tracker
     @string_interp = false
     @current_set = nil
-    @debug_mode = tracker.options[:debug]
     @current_template = @current_module = @current_class = @current_method = nil
     self.strict = false
     self.auto_shift_type = false
@@ -396,9 +395,5 @@ class Brakeman::BaseCheck < SexpProcessor
     else
       "config/environment.rb"
     end
-  end
-
-  def debug_info msg
-    Kernel.warn msg if @debug_mode
   end
 end
