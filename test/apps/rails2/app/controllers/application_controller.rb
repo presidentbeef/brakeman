976530c75a18c9079603cbd4cd43252ade05fb82
Justin Collins <jcollins@attinteractive.com>
2012-02-04 18:51:33 -0800

    Add test for weird filter

--- test/apps/rails2/app/controllers/application_controller.rb
+++ test/apps/rails2/app/controllers/application_controller.rb
@@ -7,4 +7,32 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  before_filter :awesome
+
+  def funky_panda
+  end
+
+  def awesome
+    something = if params[:thang]
+                  params[:thang]
+                elsif somevar = "monkeypanda"
+                  somevar = somevar.split(",").map { |s|
+                    s += 'stuff' unless s =~ /regex/
+                      s.split('things')
+                  }.first
+                  somevar.first.downcase
+                end
+
+    if (some_var = SomeClass.things, something)
+      AnotherClass.thang = @thang = some_var.to_sym
+    elsif (some_var = find_thang(AppConfig.stuff, something))
+      AnotherClass.thang = @thang = some_var.to_sym
+    end
+
+    if beta_override && cookies['yummy'] != @thang.to_s
+      cookies['yummy'] = { :value => @thang.to_s }
+    end
+
+    return true
+  end
 end
