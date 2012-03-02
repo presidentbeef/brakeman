94872c3efd5dc4f456d65b4c0f42b02572b271f5
Justin Collins <collins@twitter.com>
2012-03-01 18:38:58 -0800

    Add version check for SafeBuffer vulnerability
    upgrading is recommended, no workarounds
    https://groups.google.com/group/rubyonrails-security/browse_thread/thread/edd28f1e3d04e913?pli=1

---
+++ lib/brakeman/checks/check_safe_buffer_manipulation.rb
@@ -0,0 +1,30 @@
+require 'brakeman/checks/base_check'
+
+#Check for unsafe manipulation of strings
+#Right now this is just a version check for
+#https://groups.google.com/group/rubyonrails-security/browse_thread/thread/edd28f1e3d04e913?pli=1
+class Brakeman::CheckSafeBufferManipulation < Brakeman::BaseCheck
+  Brakeman::Checks.add self
+
+  @description = "Check for Rails versions with SafeBuffer bug"
+
+  def run_check
+
+    if version_between? "3.0.0", "3.0.11"
+      suggested_version = "3.0.12"
+    elsif version_between? "3.1.0", "3.1.3"
+      suggested_version = "3.1.4"
+    elsif version_between? "3.2.0", "3.2.1"
+      suggested_version = "3.2.2"
+    else
+      return
+    end
+
+    message = "Rails #{tracker.config[:rails_version]} has a vulnerabilty in SafeBuffer. Upgrade to #{suggested_version} or apply patches."
+
+    warn :warning_type => "Cross Site Scripting",
+      :message => message,
+      :confidence => CONFIDENCE[:med],
+      :file => gemfile_or_environment
+  end
+end
