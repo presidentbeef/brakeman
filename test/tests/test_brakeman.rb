697be0374f82fdc28599a13d2464c627496a7357
Justin Collins <collins@twitter.com>
2012-03-21 13:43:09 -0700

    Use old ruby_parser (2.3.1) for Ruby 1.8 parsing

--- test/tests/test_brakeman.rb
+++ test/tests/test_brakeman.rb
@@ -1,13 +1,21 @@
 class UtilTests < Test::Unit::TestCase
+  def setup
+    if RUBY_VERSION =~ /^1\.9/
+      @ruby_parser = Ruby19Parser
+    else
+      @ruby_parser = RubyParser
+    end
+  end
+
   def util
     Class.new.extend Brakeman::Util
   end
 
   def test_cookies?
-    assert util.cookies?(Ruby18Parser.new.parse 'cookies[:x][:y][:z]')
+    assert util.cookies?(@ruby_parser.new.parse 'cookies[:x][:y][:z]')
   end
 
   def test_params?
-    assert util.params?(Ruby18Parser.new.parse 'params[:x][:y][:z]')
+    assert util.params?(@ruby_parser.new.parse 'params[:x][:y][:z]')
   end
 end
