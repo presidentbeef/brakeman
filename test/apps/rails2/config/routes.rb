a0ef38d94e2fe60bee7f53196a95ca11ef96667e
Justin Collins <justin@presidentbeef.com>
2012-08-02 11:08:51 -0700

    Add test for Rails 2 resource route

--- test/apps/rails2/config/routes.rb
+++ test/apps/rails2/config/routes.rb
@@ -7,6 +7,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect 'something', :controller => "something#{dynamic}"
 
+  map.resource :users
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
