08d4eb529ec826d8777e8298f3043d20aebbad0a
Justin Collins <justin@presidentbeef.com>
2012-11-12 17:49:49 -0800

    Add test for Rails 2 named route with no args

--- test/apps/rails2/config/routes.rb
+++ test/apps/rails2/config/routes.rb
@@ -53,4 +53,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.some_routes
 end
