b8b9d39379e1ef732783cb64ae6555bb5f90a30d
Justin Collins <justin@presidentbeef.com>
2012-12-11 11:28:19 -0800

    Support "new" `validates` method for models
    when looking for bad regexes. Fixes #198

--- lib/brakeman/checks/check_validation_regex.rb
+++ lib/brakeman/checks/check_validation_regex.rb
@@ -13,22 +13,48 @@ class Brakeman::CheckValidationRegex < Brakeman::BaseCheck
   @description = "Report uses of validates_format_of with improper anchors"
 
   WITH = Sexp.new(:lit, :with)
+  FORMAT = Sexp.new(:lit, :format)
 
   def run_check
     active_record_models.each do |name, model|
       @current_model = name
       format_validations = model[:options][:validates_format_of]
+
       if format_validations
         format_validations.each do |v|
-          process_validator v
+          process_validates_format_of v
+        end
+      end
+
+      validates = model[:options][:validates]
+
+      if validates
+        validates.each do |v|
+          process_validates v
         end
       end
     end
   end
 
   #Check validates_format_of
-  def process_validator validator
+  def process_validates_format_of validator
     if value = hash_access(validator.last, WITH)
+      check_regex value, validator
+    end
+  end
+
+  #Check validates ..., :format => ...
+  def process_validates validator
+    hash_arg = validator.last
+    return unless hash? hash_arg
+
+    value = hash_access(hash_arg, FORMAT)
+
+    if hash? value
+      value = hash_access(value, WITH)
+    end
+
+    if value
       check_regex value, validator
     end
   end
