386b46f2afd3bf6b9d66a948e8731f1eeb71469d
Justin Collins <justin@presidentbeef.com>
2012-12-20 15:52:22 -0800

    Set `-a` to be default

--- lib/brakeman.rb
+++ lib/brakeman.rb
@@ -16,7 +16,7 @@ module Brakeman
   #Options:
   #
   #  * :app_path - path to root of Rails app (required)
-  #  * :assume_all_routes - assume all methods are routes (default: false)
+  #  * :assume_all_routes - assume all methods are routes (default: true)
   #  * :check_arguments - check arguments of methods (default: true)
   #  * :collapse_mass_assignment - report unprotected models in single warning (default: true)
   #  * :combine_locations - combine warning locations (default: true)
