5e2f4ce01d13e2707725456cda579c9d8c63f220
Justin Collins <justin@presidentbeef.com>
2013-04-03 18:30:14 -0700

    Add tests for quiet option

--- test/tests/test_brakeman.rb
+++ test/tests/test_brakeman.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 class UtilTests < Test::Unit::TestCase
   def setup
     @ruby_parser = RubyParser
@@ -49,5 +51,48 @@ class BaseCheckTests < Test::Unit::TestCase
 
   def test_version_between_pre_release
     assert version_between?("3.2.9.rc2", "3.2.5", "4.0.0")
+  end
+
+  def test_quiet_option_from_file
+    config = Tempfile.new("config")
+
+    config.write <<-YAML.strip
+    ---
+    :quiet: true
+    YAML
+
+    config.close
+
+    options = {
+      :config_file => config.path,
+      :app_path => "/tmp" #doesn't need to be real
+    }
+
+    final_options = Brakeman.set_options(options)
+
+    config.unlink
+
+    assert final_options[:quiet], "Expected quiet option to be true, but was #{final_options[:quiet]}"
+  end
+
+  def test_quiet_option_default
+    options = {
+      :app_path => "/tmp" #doesn't need to be real
+    }
+
+    final_options = Brakeman.set_options(options)
+
+    assert final_options[:quiet], "Expected quiet option to be true, but was #{final_options[:quiet]}"
+  end
+
+  def test_quiet_option_with_nil
+    options = {
+      :quiet => nil,
+      :app_path => "/tmp" #doesn't need to be real
+    }
+
+    final_options = Brakeman.set_options(options)
+
+    assert_nil final_options[:quiet]
   end
 end
