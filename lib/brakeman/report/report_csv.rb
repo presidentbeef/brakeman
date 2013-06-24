5fb055e6a6a6382e1409ce98c0bde459f76ed5a6
Justin Collins <justin@presidentbeef.com>
2013-06-24 00:07:18 -0700

    Refactor Report

---
+++ lib/brakeman/report/report_csv.rb
@@ -0,0 +1,56 @@
+require "csv"
+require "brakeman/report/initializers/faster_csv"
+require "brakeman/report/report_table"
+
+class Brakeman::Report::CSV < Brakeman::Report::Table
+  def generate_report
+    output = csv_header
+    output << "\nSUMMARY\n"
+
+    output << table_to_csv(generate_overview) << "\n"
+
+    output << table_to_csv(generate_warning_overview) << "\n"
+
+    #Return output early if only summarizing
+    if tracker.options[:summary_only]
+      return output
+    end
+
+    if tracker.options[:report_routes] or tracker.options[:debug]
+      output << "CONTROLLERS\n"
+      output << table_to_csv(generate_controllers) << "\n"
+    end
+
+    if tracker.options[:debug]
+      output << "TEMPLATES\n\n"
+      output << table_to_csv(generate_templates) << "\n"
+    end
+
+    res = generate_errors
+    output << "ERRORS\n" << table_to_csv(res) << "\n" if res
+
+    res = generate_warnings
+    output << "SECURITY WARNINGS\n" << table_to_csv(res) << "\n" if res
+
+    output << "Controller Warnings\n"
+    res = generate_controller_warnings
+    output << table_to_csv(res) << "\n" if res
+
+    output << "Model Warnings\n"
+    res = generate_model_warnings
+    output << table_to_csv(res) << "\n" if res
+
+    res = generate_template_warnings
+    output << "Template Warnings\n"
+    output << table_to_csv(res) << "\n" if res
+
+    output
+  end
+
+  #Generate header for CSV output
+  def csv_header
+    header = CSV.generate_line(["Application Path", "Report Generation Time", "Checks Performed", "Rails Version"])
+    header << CSV.generate_line([File.expand_path(tracker.options[:app_path]), Time.now.to_s, checks.checks_run.sort.join(", "), rails_version])
+    "BRAKEMAN REPORT\n\n" + header
+  end
+end
---
+++ lib/brakeman/report/report_table.rb
@@ -0,0 +1,108 @@
+require 'terminal-table'
+
+class Brakeman::Report::Table < Brakeman::Report::Base
+  def generate_report
+    out = text_header <<
+    "\n\n+SUMMARY+\n\n" <<
+    truncate_table(generate_overview.to_s) << "\n\n" <<
+    truncate_table(generate_warning_overview.to_s) << "\n"
+
+    #Return output early if only summarizing
+    return out if tracker.options[:summary_only]
+
+    if tracker.options[:report_routes] or tracker.options[:debug]
+      out << "\n+CONTROLLERS+\n" <<
+      truncate_table(generate_controllers.to_s) << "\n"
+    end
+
+    if tracker.options[:debug]
+      out << "\n+TEMPLATES+\n\n" <<
+      truncate_table(generate_templates.to_s) << "\n"
+    end
+
+    res = generate_errors
+    out << "+Errors+\n" << truncate_table(res.to_s) if res
+
+    res = generate_warnings
+    out << "\n\n+SECURITY WARNINGS+\n\n" << truncate_table(res.to_s) if res
+
+    res = generate_controller_warnings
+    out << "\n\n\nController Warnings:\n\n" << truncate_table(res.to_s) if res
+
+    res = generate_model_warnings
+    out << "\n\n\nModel Warnings:\n\n" << truncate_table(res.to_s) if res
+
+    res = generate_template_warnings
+    out << "\n\nView Warnings:\n\n" << truncate_table(res.to_s) if res
+
+    out << "\n"
+    out
+  end
+
+  def generate_overview
+    num_warnings = all_warnings.length
+
+    Terminal::Table.new(:headings => ['Scanned/Reported', 'Total']) do |t|
+      t.add_row ['Controllers', tracker.controllers.length]
+      t.add_row ['Models', tracker.models.length - 1]
+      t.add_row ['Templates', number_of_templates(@tracker)]
+      t.add_row ['Errors', tracker.errors.length]
+      t.add_row ['Security Warnings', "#{num_warnings} (#{warnings_summary[:high_confidence]})"]
+    end
+  end
+
+  #Generate listings of templates and their output
+  def generate_templates
+    out_processor = Brakeman::OutputProcessor.new
+    template_rows = {}
+    tracker.templates.each do |name, template|
+      unless template[:outputs].empty?
+        template[:outputs].each do |out|
+          out = out_processor.format out
+          template_rows[name] ||= []
+          template_rows[name] << out.gsub("\n", ";").gsub(/\s+/, " ")
+        end
+      end
+    end
+
+    template_rows = template_rows.sort_by{|name, value| name.to_s}
+
+    output = ''
+    template_rows.each do |template|
+      output << template.first.to_s << "\n\n"
+      table = Terminal::Table.new(:headings => ['Output']) do |t|
+        # template[1] is an array of calls
+        template[1].each do |v|
+          t.add_row [v]
+        end
+      end
+
+      output << table.to_s << "\n\n"
+    end
+
+    output
+  end
+
+  def render_array template, headings, value_array, locals
+    return if value_array.empty?
+
+    Terminal::Table.new(:headings => headings) do |t|
+      value_array.each { |value_row| t.add_row value_row }
+    end
+  end
+
+  #Generate header for text output
+  def text_header
+    <<-HEADER
+
++BRAKEMAN REPORT+
+
+Application path: #{File.expand_path tracker.options[:app_path]}
+Rails version: #{rails_version}
+Brakeman version: #{Brakeman::Version}
+Started at #{tracker.start_time}
+Duration: #{tracker.duration} seconds
+Checks run: #{checks.checks_run.sort.join(", ")}
+HEADER
+  end
+end
