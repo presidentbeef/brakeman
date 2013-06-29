48f0527906f0780a82a626180a9d5ebf05652a1e
Justin Collins <justin@presidentbeef.com>
2013-06-28 18:09:40 -0700

    Add back controller report for -m and -d

--- lib/brakeman/report/report_base.rb
+++ lib/brakeman/report/report_base.rb
@@ -31,6 +31,48 @@ class Brakeman::Report::Base
     render_array('warning_overview', ['Warning Type', 'Total'], values, locals)
   end
 
+  #Generate table of controllers and routes found for those controllers
+  def generate_controllers
+    controller_rows = []
+
+    tracker.controllers.keys.map{|k| k.to_s}.sort.each do |name|
+      name = name.to_sym
+      c = tracker.controllers[name]
+
+      if tracker.routes[:allow_all_actions] or tracker.routes[name] == :allow_all_actions
+        routes = c[:public].keys.map{|e| e.to_s}.sort.join(", ")
+      elsif tracker.routes[name].nil?
+        #No routes defined for this controller.
+        #This can happen when it is only a parent class
+        #for other controllers, for example.
+        routes = "[None]"
+
+      else
+        routes = (Set.new(c[:public].keys) & tracker.routes[name.to_sym]).
+          to_a.
+          map {|e| e.to_s}.
+          sort.
+          join(", ")
+      end
+
+      if routes == ""
+        routes = "[None]"
+      end
+
+      controller_rows << { "Name" => name.to_s,
+        "Parent" => c[:parent].to_s,
+        "Includes" => c[:includes].join(", "),
+        "Routes" => routes
+      }
+    end
+
+    cols = ['Name', 'Parent', 'Includes', 'Routes']
+
+    locals = {:controller_rows => controller_rows}
+    values = controller_rows.collect{|row| row.values_at(*cols) }
+    render_array('controller_overview', cols, values, locals)
+  end
+
   #Generate table of errors or return nil if no errors
   def generate_errors
     values = tracker.errors.collect{|error| [error[:error], error[:backtrace][0]]}
