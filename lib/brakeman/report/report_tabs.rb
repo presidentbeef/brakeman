3d21512772edd3e8366241b57b1a9386d70bdc46
Justin Collins <justin@presidentbeef.com>
2013-07-25 13:48:06 -0700

    Also ignore warnings in tabs output

--- lib/brakeman/report/report_tabs.rb
+++ lib/brakeman/report/report_tabs.rb
@@ -2,10 +2,10 @@
 #https://github.com/presidentbeef/brakeman-jenkins-plugin
 class Brakeman::Report::Tabs < Brakeman::Report::Base
   def generate_report
-    [[:warnings, "General"], [:controller_warnings, "Controller"],
+    [[:generic_warnings, "General"], [:controller_warnings, "Controller"],
       [:model_warnings, "Model"], [:template_warnings, "Template"]].map do |meth, category|
 
-      checks.send(meth).map do |w|
+      self.send(meth).map do |w|
         line = w.line || 0
         w.warning_type.gsub!(/[^\w\s]/, ' ')
         "#{warning_file(w, :absolute)}\t#{line}\t#{w.warning_type}\t#{category}\t#{w.format_message}\t#{TEXT_CONFIDENCE[w.confidence]}"
