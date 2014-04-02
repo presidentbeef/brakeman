2b5c6b3026d06a9e8f26a75dbb2986f6ca035b72
Greg Ose <greg.ose@github.com>
2014-04-02 12:43:37 -0500

    support subpath in repo

--- lib/brakeman/options.rb
+++ lib/brakeman/options.rb
@@ -198,7 +198,7 @@ module Brakeman::Options
           options[:absolute_paths] = true
         end
 
-        opts.on "--github-repo USER/REPO[@REF]", "Output links to GitHub in markdown and HTML reports using specified repo" do |repo|
+        opts.on "--github-repo USER/REPO[/PATH][@REF]", "Output links to GitHub in markdown and HTML reports using specified repo" do |repo|
           options[:github_repo] = repo
         end
 
