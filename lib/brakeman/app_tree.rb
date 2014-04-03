577e68e9e0ba57cab097b27a1d526ff37be2b190
Greg Ose <greg.ose@github.com>
2014-04-03 16:00:06 -0500

    add --additional-libs option

--- lib/brakeman/app_tree.rb
+++ lib/brakeman/app_tree.rb
@@ -15,6 +15,7 @@ module Brakeman
       if options[:only_files]
         init_options[:only_files] = Regexp.new("(?:" << options[:only_files].map { |f| Regexp.escape f }.join("|") << ")")
       end
+      init_options[:additional_libs] = options[:additional_libs]
       new(root, init_options)
     end
 
@@ -22,6 +23,7 @@ module Brakeman
       @root = root
       @skip_files = init_options[:skip_files]
       @only_files = init_options[:only_files]
+      @additional_libs = init_options[:additional_libs] || []
     end
 
     def expand_path(path)
@@ -72,13 +74,14 @@ module Brakeman
 
     def lib_paths
       @lib_files ||= find_paths("lib").reject { |path| path.include? "/generators/" } +
-                     find_paths("app/helpers") +
-                     find_paths("app/api") +
-                     find_paths("app/mailers") +
-                     find_paths("app/view_models")
+                     find_additional_lib_paths
     end
 
   private
+
+    def find_additional_lib_paths
+      @additional_libs.collect{ |path| find_paths path }.flatten
+    end
 
     def find_paths(directory, extensions = "*.rb")
       pattern = @root + "/{engines/*/,}#{directory}/**/#{extensions}"
--- lib/brakeman/version.rb
+++ lib/brakeman/version.rb
@@ -1,3 +1,3 @@
 module Brakeman
-  Version = "2.4.3github4"
+  Version = "2.4.3github5"
 end
