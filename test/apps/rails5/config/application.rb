e2d36dcada198d81dddb6c632a75504ee12d74d1
Justin Collins <justin@presidentbeef.com>
2016-01-15 16:30:18 -0800

    Add Rails 5 tests

---
+++ test/apps/rails5/config/application.rb
@@ -0,0 +1,15 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Rails5
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
