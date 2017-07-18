7e0c7c75cb183815c15ed2ae29a8a67c643b14d3
Justin Collins <justin@presidentbeef.com>
2017-07-17 18:11:31 -0700

    Update ruby2ruby

--- gem_common.rb
+++ gem_common.rb
@@ -7,7 +7,7 @@ module Brakeman
     def self.base_dependencies spec
       spec.add_dependency "ruby_parser", "~>3.9.0"
       spec.add_dependency "sexp_processor", "~> 4.7"
-      spec.add_dependency "ruby2ruby", "~>2.3.0"
+      spec.add_dependency "ruby2ruby", "~>2.4.0"
       spec.add_dependency "safe_yaml", ">= 1.0"
     end
 
