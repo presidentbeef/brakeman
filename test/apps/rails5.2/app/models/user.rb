6ff5f54b3fb629bae2ba92b52e2b3730f7c60169
Justin Collins <justin@presidentbeef.com>
2018-04-25 12:25:13 -0700

    Add test for String#freeze inside SQL

--- test/apps/rails5.2/app/models/user.rb
+++ test/apps/rails5.2/app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   def not_something thing
     where.not("blah == #{thing}")
   end
+  SUBQUERY_TABLE_ALIAS = "my_table_alias".freeze
+
+  # This is used inside a larger query by using `inner_query.to_sql`
+  def inner_query
+    self.class.
+      select("#{SUBQUERY_TABLE_ALIAS}.*").
+      from("#{table_name} AS #{SUBQUERY_TABLE_ALIAS}")
+  end
 end
