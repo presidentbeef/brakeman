6251a837a5d363cd9fc3cd4b74f10ee6a4bc7029
Justin Collins <jcollin@synopsys.com>
2019-02-22 12:49:24 -0800

    Add test for original exception in Tracker#errors

---
+++ test/tests/tracker.rb
@@ -0,0 +1,23 @@
+require_relative '../test'
+
+class TrackerTests < Minitest::Test
+  def setup
+    @tracker = Brakeman::Tracker.new(nil)
+  end
+
+  def test_exception_in_error_list
+    @tracker.error Exception.new
+
+    assert_equal 1, @tracker.errors.length
+
+    @tracker.errors.each do |e|
+      assert e.has_key? :exception
+      assert e.has_key? :error
+      assert e.has_key? :backtrace
+
+      assert e[:exception].is_a? Exception
+      assert e[:error].is_a? String
+      assert e[:backtrace].is_a? Array
+    end
+  end
+end
