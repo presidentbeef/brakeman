ce8a09f5c228103f1a0e84a90652b3ae41087dd5
Linos Giannopoulos <lgian@skroutz.gr>
2019-06-19 17:49:09 +0300

    Add tests for reverse tabnabbing check

--- lib/brakeman/checks/check_reverse_tabnabbing.rb
+++ lib/brakeman/checks/check_reverse_tabnabbing.rb
@@ -13,31 +13,41 @@ class Brakeman::CheckReverseTabnabbing < Brakeman::BaseCheck
   end
 
   def process_result result
-    return unless original? result and result[:call].third_arg
+    return unless original? result and result[:call].last_arg
 
-    html_opts = result[:call].third_arg
-    _, target = hash_access html_opts, :target
-    _, rel = hash_access html_opts, :rel
-    return unless target
+    html_opts = result[:call].last_arg
+    return unless hash? html_opts
 
-    target_url = result[:call].second_arg
+    target = hash_access html_opts, :target
+    return unless target && string?(target) && target.value == "_blank"
 
-    # `_url` and `_path` lead to urls on to the same origin.
+    target_url = result[:block] ? result[:call].first_arg : result[:call].second_arg
+
+    # `url_for` and `_path` calls lead to urls on to the same origin.
     # That means that an adversary would need to run javascript on
     # the victim application's domain. If that is the case, the adversary
     # already has the ability to redirect the victim user anywhere.
-    if target_url[0] == :call then
-      func_s = target_url.method.to_s
-      return unless !func_s.end_with?("url") && !func_s.end_with?("path")
+    # Also statically provided URLs (interpolated or otherwise) are also
+    # ignored as they produce many false positives.
+    return if !call?(target_url) || target_url.method.match(/^url_for$|_path$/)
+
+    rel = hash_access html_opts, :rel
+    confidence = :medium
+
+    if rel && string?(rel) then
+      rel = rel.value
+      return if rel.include?("noopener") && rel.include?("noreferrer")
+
+      if rel.include?("noopener") ^ rel.include?("noreferrer") then
+        confidence = :weak
+      end
     end
 
-    if target == "_blank" && (!rel || (rel && !rel.include?("noopener"))) then
-      warn :result => result,
-        :warning_type => "Reverse Tabnabbing",
-        :warning_code => :reverse_tabnabbing,
-        :message => "The newly opened tab can control the parent tab's " +
-                    "location, thus redirect it to a phishing page",
-        :confidence => :weak
-    end
+    warn :result => result,
+      :warning_type => "Reverse Tabnabbing",
+      :warning_code => :reverse_tabnabbing,
+      :message => "The newly opened tab can control the parent tab's " +
+                  "location, thus redirect it to a phishing page",
+      :confidence => confidence
   end
 end
