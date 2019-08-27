fbf1595171503c35b9c8ea30e01df67f8fe442cd
Justin Collins <jcollin@synopsys.com>
2019-08-27 09:39:22 -0700

    class name docs

--- lib/brakeman/tracker/class_collection.rb
+++ lib/brakeman/tracker/class_collection.rb
@@ -1,6 +1,7 @@
 require 'set'
 
 module Brakeman
+  # Represents a class name, including variations with different scopes.
   class ClassName
     attr_reader :names
 
@@ -44,12 +45,14 @@ module Brakeman
     end
   end
 
+  # Holds a collection of classes indexed by class name.
   class ClassCollection
     def initialize
       @class_index = {}
       @classes = []
     end
 
+    # Add class to collection by name.
     def []= name, klass
       if name.is_a? ClassName
         @class_index[name.key] = klass
@@ -60,6 +63,11 @@ module Brakeman
       @classes << klass
     end
 
+    # Look up class by name.
+    # If `strict` is `true`, only the indexed name will be used.
+    #
+    # Otherwise, if the name is not in the index, each class
+    # will attempt to match the name in a more general way.
     def [] name, strict = true
       return nil if name.nil? # TODO why are we looking up nil class names?
 
@@ -82,10 +90,12 @@ module Brakeman
       nil
     end
 
+    # Add class by name.
     def << klass
       self[klass.name] = klass
     end
 
+    # Delete class by name.
     def delete name
       deleted = @class_index.delete name
       @classes.delete(deleted)
@@ -93,6 +103,7 @@ module Brakeman
       deleted # To match Hash behavior
     end
 
+    # Iterate over all classes and delete when the block returns true.
     def delete_if &block
       @classes.delete_if do |klass|
         if yield(klass)
@@ -102,32 +113,41 @@ module Brakeman
       end
     end
 
+    # Iterate over each (name, class) pair.
     def each &block
       @class_index.each &block
     end
 
+    # Iterate over each class.
     def each_class &block
       @class_index.each_value &block
     end
 
+    # Return true if the collection has any classes.
     def any?
       raise ArgumentError if block_given?
 
       !self.empty?
     end
 
+    # Return true if the collection is empty.
     def empty?
       @classes.empty?
     end
 
+    # Returns true if the collection contains the given class name.
+    #
+    # *Fuzzy* match on class name.
     def include? class_name
       !!self[class_name, false]
     end
 
+    # Return class names in index.
     def keys
       @class_index.keys
     end
 
+    # Return number of classes in collection.
     def length
       @classes.length
     end
