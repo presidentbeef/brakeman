40ff34071e0153afd991ac4d925f59dae74e2ce2
Justin Collins <justin@presidentbeef.com>
2019-10-21 11:49:33 -0700

    Convert s(:lambda) to s(:call) in Sexp#block_call
    Fixes #1410

--- test/apps/rails6/config/routes.rb
+++ test/apps/rails6/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # https://github.com/presidentbeef/brakeman/issues/1410
+  x = ->(args) {
+  }
 end
