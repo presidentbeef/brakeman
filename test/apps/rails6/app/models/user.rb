26da3e78485d0e38e9926c6a7c5425759b594fe6
Justin Collins <justin@presidentbeef.com>
2019-11-21 15:02:18 -0800

    Find SQL injection in String#strip_heredoc
    Fixes #1433

--- test/apps/rails6/app/models/user.rb
+++ test/apps/rails6/app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   STUFF = [
     DEFAULT_PASSWORD = "p@ssw3rd2"
   ]
+
+  def self.scope_with_strip_heredoc(name)
+    conditions = <<-SQL.strip_heredoc
+      name = '#{name}'
+    SQL
+
+    where(conditions)
+  end
 end
