381bf0753a03e8a2ae31affc551be33fac6ab408
Naoki Kimura <n.kimura.cap@gmail.com>
2020-02-14 10:42:48 +0900

    Add a test

--- test/tests/junit_output.rb
+++ test/tests/junit_output.rb
@@ -6,8 +6,22 @@ class JUnitOutputTests < Minitest::Test
     @@document ||= REXML::Document.new(Brakeman.run("#{TEST_PATH}/apps/rails3.2").report.to_junit)
   end
 
+  def test_for_ignored_warnings
+    assert @@document.get_elements('/testsuites/brakeman:ignored/brakeman:warning').length == 0
+
+    document = REXML::Document.new(Brakeman.run("#{TEST_PATH}/apps/rails4").report.to_junit)
+    ignored_warnings = document.get_elements('/testsuites/brakeman:ignored/brakeman:warning')
+    assert_equal 1, ignored_warnings.length
+  end
+
   def test_for_errors
     assert @@document.get_elements('/testsuites/brakeman:errors/brakeman:error').length == 0
+
+    tracker = Brakeman.run("#{TEST_PATH}/apps/rails3.2")
+    tracker.error Exception.new "some message"
+    document = REXML::Document.new(tracker.report.to_junit)
+    elements = document.get_elements('/testsuites/brakeman:errors/brakeman:error')
+    assert_equal elements.map { |e| e.attribute('brakeman:message').to_s }, ["some message"]
   end
 
   def test_for_obsolete
