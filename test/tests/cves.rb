b9f64b282008c1acd955c4ef6585d8d71b872971
Justin Collins <justin@presidentbeef.com>
2020-08-03 09:40:36 -0700

    Add CVE-2020-8166 test for Rails 6

--- test/tests/cves.rb
+++ test/tests/cves.rb
@@ -336,6 +336,16 @@ class CVETests < Minitest::Test
 
     assert_new 0
     assert_version "5.2.4.3"
-    assert_no_warning type: :controller, :warning_code => 116
+    assert_no_warning type: :generic, :warning_code => 116
+  end
+
+  def test_CVE_2020_8166_rails6
+    before_rescan_of "Gemfile", "rails6" do
+      replace "Gemfile", "gem 'rails', '~> 6.0.0.beta2'", "gem 'rails', '~> 6.0.0'"
+    end
+
+    assert_new 1
+    assert_version "6.0.0"
+    assert_warning type: :generic, :warning_code => 116
   end
 end
