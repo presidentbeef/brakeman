024b22a35cb0a292b48a37c573a3f1d367482ee0
Justin Collins <justin@presidentbeef.com>
2020-09-28 13:26:28 -0700

    Limit Haml version to ~5.1.0
    Until issue with 5.2.0 is addressed.

--- gem_common.rb
+++ gem_common.rb
@@ -19,7 +19,7 @@ module Brakeman
       spec.add_dependency "terminal-table", "~>1.4"
       spec.add_dependency "highline", "~>2.0"
       spec.add_dependency "erubis", "~>2.6"
-      spec.add_dependency "haml", "~>5.1"
+      spec.add_dependency "haml", "~>5.1.0"
       spec.add_dependency "slim", ">=1.3.6", "<=4.1"
     end
   end
