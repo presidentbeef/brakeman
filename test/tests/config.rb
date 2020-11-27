86060d36d18f955ce8bbf9484291e1edac4dcb27
Justin Collins <justin@presidentbeef.com>
2020-11-27 10:25:55 -0800

    Handle config settings like config.load_defaults(...)
    Not just attribute settings like `config.blah = ...`

--- test/tests/config.rb
+++ test/tests/config.rb
@@ -1,9 +1,11 @@
 require_relative '../test'
 
 class RailsConfiguration < Minitest::Test
-  def test_rails5_configuration
-    tracker = Brakeman.run(File.join(TEST_PATH, "apps", "rails5"))
+  def test_rails5_2_configuration_load_defaults
+    tracker = Brakeman.run(File.join(TEST_PATH, "apps", "rails5.2"))
 
     refute tracker.config.rails.empty?
+
+    assert_equal tracker.config.rails[:load_defaults], Sexp.new(:lit, 5.2)
   end
 end
