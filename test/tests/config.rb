0b92b93e127da7c24581bbdb543a7a91c2231794
Justin Collins <justin@presidentbeef.com>
2020-12-01 10:32:53 -0800

    Add tests for handling `load_defaults(version)`

--- test/tests/config.rb
+++ test/tests/config.rb
@@ -6,6 +6,20 @@ class RailsConfiguration < Minitest::Test
 
     refute tracker.config.rails.empty?
 
-    assert_equal tracker.config.rails[:load_defaults], Sexp.new(:lit, 5.2)
+    # Settings like `config.load_defaults(5.2)` shold be included, not
+    # just settings like `config.some.attribute = ...`
+    assert_equal Sexp.new(:lit, 5.2), tracker.config.rails[:load_defaults]
+
+    # Rails 5.0 settings should be included
+    assert_equal Sexp.new(:true), tracker.config.rails[:action_controller][:per_form_csrf_tokens]
+
+    # Rails 5.1 settings should be included
+    assert_equal Sexp.new(:false), tracker.config.rails[:assets][:unknown_asset_fallback]
+
+    # Rails 5.2 settings should be included
+    assert_equal Sexp.new(:true), tracker.config.rails[:active_record][:belongs_to_required_by_default]
+
+    # Rails 6.0 settings should not be included
+    assert_nil tracker.config.rails[:action_dispatch][:use_cookies_with_metadata]
   end
 end
