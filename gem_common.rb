407bef08c41ba425e7492e3c5da3e912299e7912
Justin Collins <justin@presidentbeef.com>
2020-12-23 07:17:44 -0800

    Add rexml as a dependency
    In Ruby 3.0, REXML has been moved to a 'bundled' gem. It seems like it
    is installed by default, but Bundler wants you to declare it as a
    dependency.

--- gem_common.rb
+++ gem_common.rb
@@ -21,6 +21,7 @@ module Brakeman
       spec.add_dependency "erubis", "~>2.6"
       spec.add_dependency "haml", "~>5.1"
       spec.add_dependency "slim", ">=1.3.6", "<=4.1"
+      spec.add_dependency "rexml", "~>3.0"
     end
   end
 end
