0d2a488a3eea6b1c0923f0d23b1e2b0e8f33da65
Marek Skubela <marek.skubela@gmail.com>
2022-04-01 10:00:08 +0200

    Add basic Ruby 3.1 syntax

--- test/apps/rails7/app/controllers/application_controller.rb
+++ test/apps/rails7/app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  def anonymouns_block_argument(&)
+    Time.use_zone('CET', &)
+  end
+
+  def hash_value_omission
+    x = 1
+    y = 2
+
+    {x:, y:}
+  end
+
+  def endless_method_definition(msg) = puts "#{Time.now}: #{msg}"
+
+  def pattern_matching_parenthesis_ommission
+    [0, 1] => _, x
+    {y: 2} => y:
+
+    {x:, y:}
+  end
+
+  def pattern_matching_non_local_variable_pin
+    {timestamp: Time.now} in {timestamp: ^(Time.new(2021)..Time.new(2022))}
+  end
 end
