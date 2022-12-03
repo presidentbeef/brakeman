bd1223202da614511614a5a710e69b63fc3f8790
Justin Collins <justin@presidentbeef.com>
2022-12-03 13:08:46 -0800

    Add test for render false positive

--- test/apps/rails7/app/controllers/users_controller.rb
+++ test/apps/rails7/app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def redirect_to_last!
     redirect_to User.last!
   end
+
+  def presence
+    # Don't warn... @field is either "foo" or nil
+    @field = params[:field].presence_in(%w[foo]) || raise(ActionController::BadRequest)
+    render "admin2/fields/#{@field}"
+  end
 end
