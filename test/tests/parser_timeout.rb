35cf8a66c55a73a9108e4d3c39b6dad763ee118d
Justin Collins <justin@presidentbeef.com>
2023-01-02 14:43:49 -0800

    Lower parser timeout in test

--- test/tests/parser_timeout.rb
+++ test/tests/parser_timeout.rb
@@ -5,7 +5,7 @@ class ParserTimeoutTests < Minitest::Test
   include BrakemanTester::RescanTestHelper
 
   def test_timeout
-    before_rescan_of "lib/large_file.rb", "rails5.2", { parser_timeout: 1 } do
+    before_rescan_of "lib/large_file.rb", "rails5.2", { parser_timeout: 0.5 } do
       random_ruby = Array.new(10000) { "def x_#{rand(1000)}\nputs '#{"**" * 1000}'\nend" }.join("\n")
       write_file "lib/large_file.rb", random_ruby
     end
@@ -13,6 +13,6 @@ class ParserTimeoutTests < Minitest::Test
     assert_equal 1, @rescanner.tracker.errors.length
 
     timeout_error = @rescanner.tracker.errors.first
-    assert_match(/Parsing .* took too long \(> 1 seconds\)/, timeout_error[:error])
+    assert_match(/Parsing .* took too long \(> 0.5 seconds\)/, timeout_error[:error])
   end
 end
