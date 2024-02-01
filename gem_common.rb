a21654897561b30cfe09b24799c773e9e2a54c41
Justin Collins <presidentbeef@users.noreply.github.com>
2024-02-01 12:11:56 -0800

    Update Highline to 3.0 (#1825)
    Closes #1812

--- gem_common.rb
+++ gem_common.rb
@@ -17,7 +17,7 @@ module Brakeman
 
     def self.extended_dependencies spec
       spec.add_dependency "terminal-table", "~>1.4"
-      spec.add_dependency "highline", "~>2.0"
+      spec.add_dependency "highline", "~>3.0"
       spec.add_dependency "erubis", "~>2.6"
       spec.add_dependency "haml", "~>5.1"
       spec.add_dependency "slim", ">=1.3.6", "<=4.1"
