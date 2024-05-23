b3858a97b36069dff4165de92fcb2aa9c247a79a
Ron Shinall <81988008+ron-shinall@users.noreply.github.com>
2024-05-23 14:15:12 -0600

    Initial work for adding Rails 8

---
+++ test/tests/rails8.rb
@@ -0,0 +1,22 @@
+require_relative "../test"
+
+class Rails8Tests < Minitest::Test
+  include BrakemanTester::FindWarning
+  include BrakemanTester::CheckExpected
+
+  def report
+    @@report ||=
+      Date.stub :today, Date.parse("2024-05-13") do
+        BrakemanTester.run_scan "rails8", "Rails 8", run_all_checks: true
+      end
+  end
+
+  def expected
+    @@expected ||= {
+      controller: 0,
+      model:      0,
+      template:   0,
+      warning:    0
+    }
+  end
+end
