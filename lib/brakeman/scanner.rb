6a48cee09d3eabc10e0d954bb6c82d034ad2964a
Gabriel Zayas <g-zayas@hotmail.com>
2024-07-10 15:03:31 +0900

    Use puts for stderr output in command line

--- lib/brakeman/scanner.rb
+++ lib/brakeman/scanner.rb
@@ -406,7 +406,7 @@ class Brakeman::Scanner
 
   def report_progress(current, total, type = "files")
     return unless @options[:report_progress]
-    $stderr.print " #{current}/#{total} #{type} processed\r"
+    $stderr.puts " #{current}/#{total} #{type} processed\r"
   end
 
   def index_call_sites
