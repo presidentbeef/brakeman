c2b14e67d25400f48e4f2d841b6015d9f01bccf9
Kurt Werle <kwerle@collaborativedrug.com>
2024-11-20 16:19:56 -0800

    Skip the hackery if we are using modern haml

--- lib/brakeman/parsers/haml_embedded.rb
+++ lib/brakeman/parsers/haml_embedded.rb
@@ -25,26 +25,8 @@ RUBY
   end
 end
 
-# Haml::Filters::Coffee used to be defined as a module - now it is a class
-if defined?(Haml::Filters::Coffee) && Haml::Filters::Coffee.class == Class
-  # Fake CoffeeScript filter for Haml
-  class Haml::Filters::Coffee
-    include Haml::Filters::Base
-    extend Brakeman::FakeHamlFilter
-  end
-
-  # Fake Markdown filter for Haml
-  class Haml::Filters::Markdown
-    include Haml::Filters::Base
-    extend Brakeman::FakeHamlFilter
-  end
-
-  # Fake Sass filter for Haml
-  class Haml::Filters::Sass
-    include Haml::Filters::Base
-    extend Brakeman::FakeHamlFilter
-  end
-else
+# Haml::Filters::Coffee used to be defined as a module - now it is a class and no longer needed
+if defined?(Haml::Filters::Coffee) && Haml::Filters::Coffee.class == Module
   # Fake CoffeeScript filter for Haml
   module Haml::Filters::Coffee
     include Haml::Filters::Base
