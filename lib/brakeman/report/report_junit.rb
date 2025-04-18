3997764458d3a055c2abcf1d715a97b7bea8d6b3
Philippe Bernery <philippe@bernery.fr>
2025-04-18 15:12:01 +0200

    Rollback to using the check name as the test name
    This enables features like re-running that fails on tools like CircleCI.

--- lib/brakeman/report/report_junit.rb
+++ lib/brakeman/report/report_junit.rb
@@ -34,11 +34,7 @@ class Brakeman::Report::JUnit < Brakeman::Report::Base
 
         warnings.each { |warning|
           test_case = test_suite.add_element 'testcase'
-          test_name = "#{warning.check} - #{warning.file.relative}"
-          if warning.line
-            test_name += ":#{warning.line}"
-          end
-          test_case.add_attribute 'name', test_name
+          test_case.add_attribute 'name', warning.check
           test_case.add_attribute 'file', file.relative
           test_case.add_attribute 'line', warning.line if warning.line
           test_case.add_attribute 'time', '0'
