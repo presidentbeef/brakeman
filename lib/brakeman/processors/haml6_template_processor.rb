82587dc212ad46ead47af421a6fd55c0fd2c2890
Justin Collins <justin@presidentbeef.com>
2025-06-21 14:40:25 -0600

    More HAML 6

--- lib/brakeman/processors/haml6_template_processor.rb
+++ lib/brakeman/processors/haml6_template_processor.rb
@@ -2,13 +2,14 @@ require 'brakeman/processors/slim_template_processor'
 
 class Brakeman::Haml6TemplateProcessor < Brakeman::SlimTemplateProcessor
   HAML_UTILS = s(:colon2, s(:colon3, :Haml), :Util)
+  HAML_UTILS2 = s(:colon2, s(:const, :Haml), :Util)
   # @output_buffer = output_buffer || ActionView::OutputBuffer.new
   AV_SAFE_BUFFER = s(:or, s(:call, nil, :output_buffer), s(:call, s(:colon2, s(:const, :ActionView), :OutputBuffer), :new))
 
   def is_escaped? exp
     call? exp and
-    exp.target == HAML_UTILS and
-    (exp.method == :escape_html or exp.method == :escape_html_safe)
+      (exp.target == HAML_UTILS or exp.target == HAML_UTILS2) and
+      (exp.method == :escape_html or exp.method == :escape_html_safe)
   end
 
   def output_buffer? exp
