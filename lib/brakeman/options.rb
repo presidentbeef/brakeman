317d2ebb08046929b09569f5070fa83e1e8c36ce
Justin Collins <justin@presidentbeef.com>
2026-02-18 20:13:04 -0700

    Older Rubies don't support ranges for options

--- lib/brakeman/options.rb
+++ lib/brakeman/options.rb
@@ -63,7 +63,11 @@ module Brakeman::Options
           options[:exit_on_error] = exit_on_error
         end
 
-        opts.on "--ensure-latest [DAYS]", Integer, "Fail when Brakeman is outdated. Optionally set minimum age in days.", (1..15) do |days|
+        opts.on "--ensure-latest [DAYS]", Integer, "Fail when Brakeman is outdated. Optionally set minimum age in days." do |days|
+          if days and not (1..15).include? days
+            raise OptionParser::InvalidArgument
+          end
+
           options[:ensure_latest] = days || true
         end
 
