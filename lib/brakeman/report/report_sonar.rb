45c767f2d83198d42d57bd1c7080eac01c0bf570
fangxing <fangxing204@gmail.com>
2026-08-13 23:57:02 +0300

    Update Sonar report format

--- lib/brakeman/report/report_sonar.rb
+++ lib/brakeman/report/report_sonar.rb
@@ -1,38 +1,72 @@
 class Brakeman::Report::Sonar < Brakeman::Report::Base
   def generate_report
+    warnings = all_warnings
     report_object = {
-      issues: all_warnings.map { |warning| issue_json(warning) }
+      rules: rules_json(warnings),
+      issues: warnings.map { |warning| issue_json(warning) }
     }
-    return JSON.pretty_generate report_object
+    JSON.pretty_generate report_object
   end
-  
+
   private
-  
+
+  def rules_json(warnings)
+    warnings
+      .each_with_object({}) do |w, unique_warnings|
+        unique_warnings[[w.warning_code, w.confidence]] ||= w
+      end
+      .values
+      .map do |w|
+        {
+          id: rule_id(w),
+          name: w.warning_type.to_s,
+          description: w.link,
+          engineId: "Brakeman",
+          cleanCodeAttribute: "TRUSTWORTHY",
+          type: "VULNERABILITY",
+          severity: rule_severity_for(w.confidence),
+          impacts: [
+            {
+              softwareQuality: "SECURITY",
+              severity: impact_severity_for(w.confidence)
+            }
+          ]
+        }
+      end
+  end
+
   def issue_json(warning)
     {
-      engineId: "Brakeman",
-      ruleId: warning.warning_code,
-      type: "VULNERABILITY",
-      severity: severity_level_for(warning.confidence),
+      ruleId: rule_id(warning),
+      effortMinutes: (4 - warning.confidence) * 15,
       primaryLocation: {
         message: warning.message,
         filePath: warning.file.relative,
         textRange: {
-          "startLine": warning.line || 1,
-          "endLine": warning.line || 1,
+          startLine: warning.line || 1,
+          endLine: warning.line || 1
         }
-      },
-      effortMinutes: (4 - warning.confidence) * 15
+      }
     }
   end
 
-  def severity_level_for(confidence)
-    if confidence == 0
-      "CRITICAL"
-    elsif confidence == 1
-      "MAJOR"
-    else
-      "MINOR"
+  def rule_id(warning)
+    "BRAKEMAN_#{warning.warning_code}_#{rule_severity_for(warning.confidence)}"
+  end
+
+  def rule_severity_for(confidence)
+    case confidence
+    when 0 then "CRITICAL"
+    when 1 then "MAJOR"
+    else "MINOR"
+    end
+  end
+
+  def impact_severity_for(confidence)
+    case confidence
+    when 0 then "HIGH"
+    when 1 then "MEDIUM"
+    else "LOW"
     end
   end
 end
