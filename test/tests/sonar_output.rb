a0b451994283f9cae4b1eaed15851ac9e490e608
fangxing <fangxing204@gmail.com>
2026-09-18 08:54:57 +0300

    Update Sonar report format (#2038)

--- test/tests/sonar_output.rb
+++ test/tests/sonar_output.rb
@@ -3,19 +3,60 @@ require 'json'
 
 class SonarOutputTests < Minitest::Test
   def setup
-    @@sonar ||= JSON.parse(Brakeman.run("#{TEST_PATH}/apps/rails3.2").report.to_sonar)
+    @@sonar ||= JSON.parse(Brakeman.run("#{TEST_PATH}/apps/rails8").report.to_sonar)
   end
 
-  def test_for_expected_keys
-    assert (@@sonar.keys - ["issues"]).empty?
+  def test_for_expected_top_level_keys
+    assert_equal ["issues", "rules"].sort, @@sonar.keys.sort
+  end
+
+  def test_for_rules_keys
+    required = ["id", "name", "description", "engineId", "cleanCodeAttribute", "type", "severity", "impacts"]
+    @@sonar["rules"].each do |rule|
+      assert_equal [], required - rule.keys, "Rule missing keys: #{rule['id']}"
+    end
+  end
+
+  def test_rules_engine_id
+    @@sonar["rules"].each do |rule|
+      assert_equal "Brakeman", rule["engineId"]
+    end
+  end
+
+  def test_rules_impacts
+    @@sonar["rules"].each do |rule|
+      assert rule["impacts"].is_a?(Array), "impacts should be an Array"
+      rule["impacts"].each do |impact|
+        assert impact.key?("softwareQuality")
+        assert impact.key?("severity")
+      end
+    end
+  end
+
+  def test_rule_ids_are_unique
+    ids = @@sonar["rules"].map { |r| r["id"] }
+    assert_equal ids.uniq, ids
   end
 
   def test_for_issues_keys
-    issues_keys = ["engineId", "ruleId", "severity", "type", "primaryLocation", "effortMinutes"]
-    @@sonar["issues"].each do |warning|
-      assert (warning.keys - issues_keys).empty?
+    required = ["ruleId", "effortMinutes", "primaryLocation"]
+    @@sonar["issues"].each do |issue|
+      assert_equal [], required - issue.keys, "Issue missing keys"
     end
-    
   end
 
+  def test_issues_reference_valid_rules
+    rule_ids = @@sonar["rules"].map { |r| r["id"] }
+    @@sonar["issues"].each do |issue|
+      assert_includes rule_ids, issue["ruleId"], "Issue references unknown ruleId: #{issue['ruleId']}"
+    end
+  end
+
+  def test_issues_have_no_engine_id_or_type_or_severity
+    @@sonar["issues"].each do |issue|
+      assert !issue.key?("engineId"), "Issues should not have engineId (moved to rules)"
+      assert !issue.key?("type"), "Issues should not have type (moved to rules)"
+      assert !issue.key?("severity"), "Issues should not have severity (moved to rules)"
+    end
+  end
 end
